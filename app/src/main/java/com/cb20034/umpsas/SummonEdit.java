3eb2eb71aa018b855ff67a66c4b4d20e9722e15d
abdurrahman <arahmanmh15@gmail.com>
2023-12-31 23:57:44 +0800

    add admin search vehicle functionFixed)

--- app/src/main/java/com/cb20034/umpsas/SummonEdit.java
+++ app/src/main/java/com/cb20034/umpsas/SummonEdit.java
@@ -1,5 +1,6 @@
 package com.cb20034.umpsas;
 
+import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -9,12 +10,14 @@ import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -35,11 +38,14 @@ import com.google.firebase.storage.UploadTask;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class SummonEdit extends AppCompatActivity {
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_IMAGE_PICK = 2;
 
-    private EditText plateNumberEditText, offenceEditText, locationEditText, fineAmountEditText;
+    private EditText plateNumberEditText, offenceEditText, locationEditText, fineAmountEditText, editDateSummon;
     private ImageView editedAddedPictureImageView;
     private Button confirmEditSummonButton, cancelEditSummonButton;
     private FloatingActionButton editFabAddPictureSummon;
@@ -72,6 +78,14 @@ public class SummonEdit extends AppCompatActivity {
         confirmEditSummonButton = findViewById(R.id.confirmEditSummon);
         cancelEditSummonButton = findViewById(R.id.cancelEditSummon);
         editFabAddPictureSummon = findViewById(R.id.editFabAddPictureSummon);
+        editDateSummon = findViewById(R.id.editDateSummon);
+
+        //old field to edit
+        plateNumberEditText.setText(selectedSummon.getPlateNumber());
+        offenceEditText.setText(selectedSummon.getOffence());
+        locationEditText.setText(selectedSummon.getLocation());
+        fineAmountEditText.setText(selectedSummon.getFineAmount());
+
 
         cameraLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
@@ -113,13 +127,43 @@ public class SummonEdit extends AppCompatActivity {
                 finish(); // Close the activity
             }
         });
+        editDateSummon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog();
+            }
+        });
     }
+    private void showDatePickerDialog() {
+        // Get the current date
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
 
+        // Create a date picker dialog
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                this,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        // Set the selected date to the text field
+                        String selectedDate = dayOfMonth + "/" + (month + 1) + "/" + year;
+                        editDateSummon.setText(selectedDate);
+                    }
+                },
+                year, month, day
+        );
+
+        // Show the date picker dialog
+        datePickerDialog.show();
+    }
     private void confirmEditSummon() {
         String plateNumber = plateNumberEditText.getText().toString().trim();
         String offence = offenceEditText.getText().toString().trim();
         String location = locationEditText.getText().toString().trim();
         String fineAmount = fineAmountEditText.getText().toString().trim();
+        String date = editDateSummon.getText().toString().trim();
 
         if (plateNumber.isEmpty() || offence.isEmpty() || location.isEmpty() || fineAmount.isEmpty()) {
             Toast.makeText(this, "All fields must be filled", Toast.LENGTH_SHORT).show();
@@ -129,13 +173,15 @@ public class SummonEdit extends AppCompatActivity {
         // Additional validations if needed
 
         // Update summon details in Firestore
-        updateSummonDetails(plateNumber, offence, location, fineAmount);
+        updateSummonDetails(plateNumber, offence, location, fineAmount, date);
     }
 
-    private void updateSummonDetails(String plateNumber, String offence, String location, String fineAmount) {
-        // Assuming you pass the selected summon's ID and user ID from the previous activity
-        String summonId = getIntent().getStringExtra("selectedSummonId");
-        String selectedUserId = getIntent().getStringExtra("selectedUserId");
+    private void updateSummonDetails(String plateNumber, String offence, String location, String fineAmount,String date) {
+        Summon selectedSummon = (Summon) getIntent().getSerializableExtra("selectedSummon");
+
+        String summonId = selectedSummon.getTimestamp();
+        String selectedUserId = selectedSummon.getUserId();
+        String oldImagePath = selectedSummon.getImagePath();
 
         if (summonId != null && selectedUserId != null) {
             DocumentReference summonRef = firestore.collection("users").document(selectedUserId)
@@ -145,13 +191,14 @@ public class SummonEdit extends AppCompatActivity {
                     "plateNumber", plateNumber,
                     "offence", offence,
                     "location", location,
-                    "fineAmount", fineAmount
+                    "fineAmount", fineAmount,
+                    "date", date
                     // Add more fields as needed
             ).addOnSuccessListener(new OnSuccessListener<Void>() {
                 @Override
                 public void onSuccess(Void unused) {
                     // Update successful
-                    uploadImage(summonId); // Upload image after updating summon details
+                    uploadImage(summonId, oldImagePath, selectedUserId); // Upload image after updating summon details
                 }
             }).addOnFailureListener(new OnFailureListener() {
                 @Override
@@ -165,7 +212,7 @@ public class SummonEdit extends AppCompatActivity {
 
 
 
-    private void uploadImage(String summonId) {
+    private void uploadImage(String summonId, String oldImagePath, String userId) {
         if (imageUri != null) {
             StorageReference storageRef = storage.getReference();
             StorageReference imageRef = storageRef.child("summon_images/" + summonId);
@@ -183,6 +230,8 @@ public class SummonEdit extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             // Image upload successful
                             Toast.makeText(SummonEdit.this, "Summon details updated successfully", Toast.LENGTH_SHORT).show();
+                            deleteOldImage(oldImagePath);
+                            getDownloadUrlAndSaveSummon(userId, summonId, imageRef);
                             finish(); // Close the activity
                         } else {
                             // Handle failure
@@ -197,6 +246,51 @@ public class SummonEdit extends AppCompatActivity {
             // No image selected, just finish the activity
             Toast.makeText(this, "Summon details updated successfully", Toast.LENGTH_SHORT).show();
             finish(); // Close the activity
+        }
+    }
+    private void getDownloadUrlAndSaveSummon(final String userId, final String timestamp, StorageReference storageReference) {
+        storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                // Update the summon with the image path
+                firestore.collection("users").document(userId)
+                        .collection("summons").document(timestamp)
+                        .update("imagePath", uri.toString())
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(SummonEdit.this, "Summon Edited successfully!", Toast.LENGTH_SHORT).show();
+
+                                    // You can also navigate to another activity, finish the current activity, etc.
+                                    // Example: Navigate to the main activity
+                                    Intent intent = new Intent(SummonEdit.this, SummonList.class);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    Toast.makeText(SummonEdit.this, "Summon update failed!", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+    }
+    private void deleteOldImage(String oldImagePath) {
+        if (oldImagePath != null && !oldImagePath.isEmpty()) {
+            StorageReference storageRef = storage.getReferenceFromUrl(oldImagePath);
+            storageRef.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    // Old image deleted successfully
+                    Log.d("TAG", "Old image deleted successfully");
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(Exception e) {
+                    // Handle failure
+                    Log.e("TAG", "Failed to delete old image: " + e.getMessage());
+                }
+            });
         }
     }
 
